be1b2d496720300578a9ed68a7e6cb1858987991
Kevin King <kevin.king@tmz.com>
2023-12-11 14:34:09 -0800

    test

--- lib/search_screen.dart
+++ lib/search_screen.dart
@@ -5,16 +5,47 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search MAM'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.settings), onPressed: () {/* Account settings */}),
-          // Add more icons if needed
+        backgroundColor: Color(0xFF4a4a4a), // The background color
+        elevation: 0, // Removes the shadow
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('assets/images/logo_trans.png'), // Your logo asset
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle), // Placeholder for user avatar
+            onPressed: () {
+              // TODO: Implement user profile navigation
+            },
+          ),
         ],
-        // Add logo and search box
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search MAM',
+            suffixIcon: Icon(Icons.search),
+            border: InputBorder.none,
+          ),
+          style: TextStyle(color: Colors.white),
+          // TODO: Add search functionality
+        ),
       ),
       body: Column(
         children: <Widget>[
-          // Utility bar goes here
+          // Top search bar and navigation menu
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            color: Color(0xFF4a4a4a),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                navItem('Images', context),
+                navItem('Collections', context),
+                navItem('Status', context),
+                navItem('Admin', context),
+              ],
+            ),
+          ),
+          // Main content area
           Expanded(
             child: GridView.builder(
               // Grid builder to build asset cards
@@ -47,7 +78,22 @@ class SearchScreen extends StatelessWidget {
           ],
         ),
       ),
-      // Implement drawers for sidebars if needed
     );
   }
+
+  Widget navItem(String title, BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        // TODO: Add navigation functionality
+      },
+      child: Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(home: SearchScreen()));
 }
