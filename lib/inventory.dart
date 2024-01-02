af72b8f225560e166d089affe0159b477d72618d
Kevin Lawrence King <your_email@example.com>
2024-01-02 11:30:15 -0800

    fixed

--- lib/inventory.dart
+++ lib/inventory.dart
@@ -1,87 +1,99 @@
 
 class Inventory {
-final int? id;
-final String type;
-final DateTime? created;
-final DateTime? lastUpdated;
-final String name;
-final int? version;
+final int id;
+final String? assetType;
+final DateTime? dateCreated;
+final DateTime? dateUpdated;
+final String? name;
+final String? createdBy;
+final String? dateLocked;
 final int? metadataID;
-final bool truncatedFlag;
-final bool? thumbnailAssetFlag;
-final int? filesize;
-final String fileExtension;
-final int? width;
-final int? checksum;
-final int? height;
-final bool versionFlag;
-final DateTime? dateLocked;
-final bool searchableFlag;
-final DateTime? sourceDateCreated;
-final int? imageContentID;
-final bool placeholder;
+final bool? placeholder;
+final String? inventoryKey;
+final String? inventoryTypeName;
 final String? restoreKey;
-final String sourceFilePath;
-final String status;
-final bool? deleteFlag;
-final String thumbnail;
+final bool? searchableFlag;
+final DateTime? sourceDateCreated;
+final String? sourceFilePath;
+final String? status;
+final String? checksum;
+final String? fileExtension;
+final int? filesize;
+final int? height;
+final int? width;
+final bool? thumbnailAssetFlag;
+final int? thumbnailid;
+final bool? truncatedFlag;
+final String? uuid;
+final bool? versionFlag;
+final int? imageContentID;
+final String? thumbnail;
+final String? systemKeywords;
 
 
 Inventory({
-this.id,
-required this.type,
-this.created,
-this.lastUpdated,
-required this.name,
-this.version,
-this.metadataID,
-required this.truncatedFlag,
-this.thumbnailAssetFlag,
-this.filesize,
-required this.fileExtension,
-this.width,
-this.checksum,
-this.height,
-required this.versionFlag,
+required this.id,
+this.assetType,
+this.dateCreated,
+this.dateUpdated,
+this.name,
+this.createdBy,
 this.dateLocked,
-required this.searchableFlag,
-this.sourceDateCreated,
-this.imageContentID,
-required this.placeholder,
+this.metadataID,
+this.placeholder,
+this.inventoryKey,
+this.inventoryTypeName,
 this.restoreKey,
-required this.sourceFilePath,
-required this.status,
-this.deleteFlag,
-required this.thumbnail,
+this.searchableFlag,
+this.sourceDateCreated,
+this.sourceFilePath,
+this.status,
+this.checksum,
+this.fileExtension,
+this.filesize,
+this.height,
+this.width,
+this.thumbnailAssetFlag,
+this.thumbnailid,
+this.truncatedFlag,
+this.uuid,
+this.versionFlag,
+this.imageContentID,
+this.thumbnail,
+this.systemKeywords,
 });
 
 factory Inventory.fromJson(Map<String, dynamic> json) {
     return Inventory(
-id: json['id'] as int?,
-type: json['type'] as String,
-created: json['created'] != null ? DateTime.parse(json['created']) : null,
-lastUpdated: json['lastUpdated'] != null ? DateTime.parse(json['lastUpdated']) : null,
-name: json['name'] as String,
-version: json['version'] as int?,
+id: json['id'] as int,
+assetType: json['assetType'] as String?,
+dateCreated: json['dateCreated'] != null ? DateTime.parse(json['dateCreated']) : null,
+dateUpdated: json['dateUpdated'] != null ? DateTime.parse(json['dateUpdated']) : null,
+name: json['name'] as String?,
+createdBy: json['createdBy'] as String?,
+dateLocked: json['dateLocked'] as String?,
 metadataID: json['metadataID'] as int?,
-truncatedFlag: json['truncatedFlag'] as bool,
-thumbnailAssetFlag: json['thumbnailAssetFlag'] as bool?,
-filesize: json['filesize'] as int?,
-fileExtension: json['fileExtension'] as String,
-width: json['width'] as int?,
-checksum: json['checksum'] as int?,
-height: json['height'] as int?,
-versionFlag: json['versionFlag'] as bool,
-dateLocked: json['dateLocked'] != null ? DateTime.parse(json['dateLocked']) : null,
-searchableFlag: json['searchableFlag'] as bool,
-sourceDateCreated: json['sourceDateCreated'] != null ? DateTime.parse(json['sourceDateCreated']) : null,
-imageContentID: json['imageContentID'] as int?,
-placeholder: json['placeholder'] as bool,
+placeholder: json['placeholder'] as bool?,
+inventoryKey: json['inventoryKey'] as String?,
+inventoryTypeName: json['inventoryTypeName'] as String?,
 restoreKey: json['restoreKey'] as String?,
-sourceFilePath: json['sourceFilePath'] as String,
-status: json['status'] as String,
-deleteFlag: json['deleteFlag'] as bool?,
-thumbnail: json['thumbnail'] as String,
+searchableFlag: json['searchableFlag'] as bool?,
+sourceDateCreated: json['sourceDateCreated'] != null ? DateTime.parse(json['sourceDateCreated']) : null,
+sourceFilePath: json['sourceFilePath'] as String?,
+status: json['status'] as String?,
+checksum: json['checksum'] as String?,
+fileExtension: json['fileExtension'] as String?,
+filesize: json['filesize'] as int?,
+height: json['height'] as int?,
+width: json['width'] as int?,
+thumbnailAssetFlag: json['thumbnailAssetFlag'] as bool?,
+thumbnailid: json['thumbnailid'] as int?,
+truncatedFlag: json['truncatedFlag'] as bool?,
+uuid: json['uuid'] as String?,
+versionFlag: json['versionFlag'] as bool?,
+imageContentID: json['imageContentID'] as int?,
+thumbnail: json['thumbnail'] as String?,
+systemKeywords: json['systemKeywords'] as String?,
 
     );
   }
