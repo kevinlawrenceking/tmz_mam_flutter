1a452a8eb769ef82306b5dd211092de1655d9a8e
Kevin King <kevin.king@tmz.com>
2024-01-11 11:53:45 -0800

    test

--- lib/inventory.dart
+++ lib/inventory.dart
@@ -1,93 +1,103 @@
 class Inventory {
-final int id;
-final String? assetType;
-final DateTime? dateCreated;
-final DateTime? dateUpdated;
-final String name;
-final String? createdBy;
-final String? dateLocked;
-final int? metadataID;
-final bool? placeholder;
-final String? inventoryKey;
-final String? inventoryTypeName;
-final String? restoreKey;
-final bool? searchableFlag;
-final DateTime? sourceDateCreated;
-final String? sourceFilePath;
-final String? status;
-final String? checksum;
-final String? fileExtension;
-final int? filesize;
-final int? height;
-final int? width;
-final bool? thumbnailAssetFlag;
-final int? thumbnailid;
-final bool? truncatedFlag;
-final String? uuid;
-final bool? versionFlag;
-final int? imageContentID;
-final String thumbnail;
-final String? systemKeywords;
-final List<Map<String, String?>> metadata;
+  final int id;
+  final String? assetType;
+  final DateTime? dateCreated;
+  final DateTime? dateUpdated;
+  final String name;
+  final String? createdBy;
+  final String? dateLocked;
+  final int? metadataID;
+  final bool? placeholder;
+  final String? inventoryKey;
+  final String? inventoryTypeName;
+  final String? restoreKey;
+  final bool? searchableFlag;
+  final DateTime? sourceDateCreated;
+  final String? sourceFilePath;
+  final String? status;
+  final String? checksum;
+  final String? fileExtension;
+  final int? filesize;
+  final int? height;
+  final int? width;
+  final bool? thumbnailAssetFlag;
+  final int? thumbnailid;
+  final bool? truncatedFlag;
+  final String? uuid;
+  final bool? versionFlag;
+  final int? imageContentID;
+  final String thumbnail;
+  final String? systemKeywords;
+  List<Map<String, String?>> metadata;
 
   Inventory({
- required this.id,
-this.assetType = "-",
-this.dateCreated,
-this.dateUpdated,
-this.name  = "-",
-this.createdBy,
-this.dateLocked,
-this.metadataID,
-this.placeholder,
-this.inventoryKey,
-this.inventoryTypeName,
-this.restoreKey,
-this.searchableFlag,
-this.sourceDateCreated,
-this.sourceFilePath,
-this.status,
-this.checksum,
-this.fileExtension,
-this.filesize,
-this.height,
-this.width,
-this.thumbnailAssetFlag,
-this.thumbnailid,
-this.truncatedFlag,
-this.uuid,
-this.versionFlag,
-this.imageContentID,
-this.thumbnail = "Unknown",
-this.systemKeywords,
+    required this.id,
+    this.assetType = "-",
+    this.dateCreated,
+    this.dateUpdated,
+    required this.name,
+    this.createdBy,
+    this.dateLocked,
+    this.metadataID,
+    this.placeholder,
+    this.inventoryKey,
+    this.inventoryTypeName,
+    this.restoreKey,
+    this.searchableFlag,
+    this.sourceDateCreated,
+    this.sourceFilePath,
+    this.status,
+    this.checksum,
+    this.fileExtension,
+    this.filesize,
+    this.height,
+    this.width,
+    this.thumbnailAssetFlag,
+    this.thumbnailid,
+    this.truncatedFlag,
+    this.uuid,
+    this.versionFlag,
+    this.imageContentID,
+    required this.thumbnail,
+    this.systemKeywords,
     required this.metadata,
   });
 
- factory Inventory.fromJson(Map<String, dynamic> json) {
-    return Inventory(
+  factory Inventory.fromJson(Map<String, dynamic> json) {
+    var inventory = Inventory(
       id: json['id'] as int? ?? 0,
-      name: json['name'] ?? '', // Provide a default value
-      metadataID: json['metadataID'] as int? ?? 0, // Provide a default value
-      truncatedFlag: json['truncatedFlag'] ?? false, // Provide a default value
-      thumbnailAssetFlag: json['thumbnailAssetFlag'] ?? false, // Provide a default value
-      filesize: json['filesize'] ?? 0, // Provide a default value
-      fileExtension: json['fileExtension'] ?? '', // Provide a default value
-      width: json['width'] ?? 0, // Provide a default value
-      checksum: json['checksum'] ?? '', // Provide a default value
-      height: json['height'] ?? 0, // Provide a default value
-      versionFlag: json['versionFlag'] ?? false, // Provide a default value
-      dateLocked: json['dateLocked'], // Nullable
-      searchableFlag: json['searchableFlag'] ?? false, // Provide a default value
+      name: json['name'] ?? '',
+      metadataID: json['metadataID'] as int? ?? 0,
+      truncatedFlag: json['truncatedFlag'] ?? false,
+      thumbnailAssetFlag: json['thumbnailAssetFlag'] ?? false,
+      filesize: json['filesize'] ?? 0,
+      fileExtension: json['fileExtension'] ?? '',
+      width: json['width'] ?? 0,
+      checksum: json['checksum'] ?? '',
+      height: json['height'] ?? 0,
+      versionFlag: json['versionFlag'] ?? false,
+      dateLocked: json['dateLocked'],
+      searchableFlag: json['searchableFlag'] ?? false,
       sourceDateCreated: json['sourceDateCreated'] != null ? DateTime.parse(json['sourceDateCreated']) : null,
-      imageContentID: json['imageContentID'] ?? 0, // Provide a default value
-      placeholder: json['placeholder'] ?? false, // Provide a default value
-      restoreKey: json['restoreKey'], // Nullable
-      sourceFilePath: json['sourceFilePath'] ?? '', // Provide a default value
-      status: json['status'] ?? '', // Provide a default value
-      thumbnail: json['thumbnail'] ?? '', // Provide a default value
+      imageContentID: json['imageContentID'] ?? 0,
+      placeholder: json['placeholder'] ?? false,
+      restoreKey: json['restoreKey'],
+      sourceFilePath: json['sourceFilePath'] ?? '',
+      status: json['status'] ?? '',
+      thumbnail: json['thumbnail'] ?? '',
       metadata: json['metadata'] != null
         ? (json['metadata'] as List).map((item) => Map<String, String?>.from(item as Map)).toList()
-        : <Map<String, String?>>[], // Provide an empty list if metadata is null
+        : <Map<String, String?>>[],
     );
+
+    // Sort metadata here
+    inventory.sortMetadata();
+
+    return inventory;
+  }
+
+  // Method to sort metadata
+  void sortMetadata() {
+    metadata.sort((a, b) => int.parse(a['orderno'] ?? '0').compareTo(int.parse(b['orderno'] ?? '0')));
   }
 }
--- lib/search_screen.dart
+++ lib/search_screen.dart
@@ -13,7 +13,14 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   Future<List<Inventory>> fetchInventory() async {
     var apiService = ApiService(baseUrl: 'http://tmztoolsdev:3000');
-    return apiService.fetchInventory(); // Replace with actual method
+    List<Inventory> inventoryList = await apiService.fetchInventory(); // Replace with actual method
+
+    // Sort metadata for each inventory item
+    for (var item in inventoryList) {
+      item.sortMetadata();
+    }
+
+    return inventoryList;
   }
 
   bool isRightPanelOpen = false;
