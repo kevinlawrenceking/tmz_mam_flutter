41b8da7e0e8737b01765c05de0e4de138dd96c41
Kevin Lawrence King <your_email@example.com>
2024-01-28 16:37:14 -0800

    V1.9 working

--- lib/flutter_flow/flutter_flow_util.dart
+++ lib/flutter_flow/flutter_flow_util.dart
@@ -23,7 +23,7 @@ export 'dart:convert' show jsonEncode, jsonDecode;
 export 'package:intl/intl.dart';
 export 'package:page_transition/page_transition.dart';
 export 'custom_icons.dart' show FFIcons;
-export 'nav/nav.dart';
+
 
 T valueOrDefault<T>(T? value, T defaultValue) =>
     (value is String && value.isEmpty) || value == null ? defaultValue : value;
