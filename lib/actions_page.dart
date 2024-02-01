a16865847bad28de6104795451c579ee62e4ed5c
Kevin King <39280670+kevinlawrenceking@users.noreply.github.com>
2024-01-31 22:54:03 -0800

    Menu works!

--- lib/actions_page.dart
+++ lib/actions_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tmz_mam_flutter/themeprovider.dart'; // Import your ThemeProvider
+import 'package:tmz_mam_flutter/components/custom_app_bar.dart';
+import 'package:tmz_mam_flutter/components/bottom_buttons_widget.dart';
+import 'package:tmz_mam_flutter/flutter_flow_theme.dart';
+
+
+
 
 class ActionsPage extends StatefulWidget {
   final String title;
@@ -13,8 +21,19 @@ class _ActionsPageState extends State<ActionsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+           appBar: CustomAppBar(
+  title: 'TMZ Media Asset Manager', // Pass a String directly
+  actions: [
+    IconButton(
+      icon: Icon(Icons.brightness_6),
+            onPressed: () {
+              // Use Provider to toggle the theme
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+    ),
+    
+    // Additional actions can be added here
+        ],
       ),
       body: Center(
         child: Column(
@@ -29,7 +48,7 @@ class _ActionsPageState extends State<ActionsPage> {
             ),
             SizedBox(height: 16.0),
             Text(
-              'Subheading based on ${widget.title}',
+              'Coming soon!', // Updated subheading text
               style: TextStyle(
                 fontSize: 16.0,
               ),
