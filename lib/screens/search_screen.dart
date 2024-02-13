010a33934a5a48f5513e7219b89b3d523e3819d3
Kevin King <39280670+kevinlawrenceking@users.noreply.github.com>
2024-02-13 11:19:18 -0800

    Removed tolist - search_screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -210,7 +210,7 @@ class SearchScreenState extends State<SearchScreen> {
                         ],
                       ),
                     );
-                  }).toList(),
+                  }),
                 ],
               ),
             ),
